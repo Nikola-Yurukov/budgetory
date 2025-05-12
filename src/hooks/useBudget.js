71a8407a230ebd6d21515f7261a29363bc4109f1
Nikola-Yurukov <nik.yurukov@mail.com>
2025-05-12 13:34:32 +0300

    fixed import

--- src/hooks/useBudget.js
+++ src/hooks/useBudget.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { defaultBudget } from '../constants/budget';
+import { calculateTotals } from '../utils/budgetUtils';
 
 export const useBudget = () => {
   const [budget] = useState(defaultBudget);
